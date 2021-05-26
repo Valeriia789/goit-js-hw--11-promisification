5034e18020b61e0b62e7524338231998e007fc55
Valeriia <valeriia.air@gmail.com>
2021-05-26 18:37:48 +0300

    функция toggleUserState() принимает два параметра allUsers и userName и возвращает промис

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './sass/main.scss';
-import './js/delay.js';
-// import './js/toggleUserState.js';
+// import './js/delay.js';
+import './js/toggleUserState.js';
 // import './js/makeTransaction.js';
--- src/js/toggleUserState.js
+++ src/js/toggleUserState.js
@@ -5,12 +5,18 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map(user =>
-    user.name === userName ? { ...user, active: !user.active } : user,
-  );
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
 
-  callback(updatedUsers);
+//   callback(updatedUsers);
+// };
+
+const toggleUserState = (allUsers, userName) => {
+  return Promise.resolve(
+    allUsers.map(user => (user.name === userName ? { ...user, active: !user.active } : user)),
+  );
 };
 
 const logger = updatedUsers => console.table(updatedUsers);
@@ -18,11 +24,11 @@ const logger = updatedUsers => console.table(updatedUsers);
 /*
  * Сейчас работает так
  */
-toggleUserState(users, 'Mango', logger);
-toggleUserState(users, 'Lux', logger);
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
 
 /*
  * Должно работать так
- */
+//  */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
